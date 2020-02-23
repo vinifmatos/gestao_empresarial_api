6f343763d2c64719782e912c1f8083f07c877792
vinifmatos <viniciusfreire4@gmail.com>
2020-02-23 15:32:12 -0300

    cria teste das requests do fornecedor

--- spec/requests/fornecedores_spec.rb
+++ spec/requests/fornecedores_spec.rb
@@ -9,4 +9,56 @@ RSpec.describe 'Fornecedores', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  before(:all) do
+    create_list(:fornecedor, 10)
+  end
+
+  describe 'GET /fornecedores' do
+    it 'retorna código 200 e um array com os lançamentos' do
+      get fornecedores_path
+      expect(response).to have_http_status(200)
+      expect(response.body).to include_json(Fornecedor.all.as_json)
+    end
+  end
+
+  describe 'GET /fornecedores/:id' do
+    it 'retorna código 200 e um json com o fornecedor' do
+      fornecedor = Fornecedor.first
+      get fornecedor_path(fornecedor)
+      expect(response).to have_http_status(200)
+      expect(response.body).to include_json(fornecedor.as_json)
+    end
+  end
+
+  describe 'POST /fornecedores' do
+    it 'retorna código 201 e um json com fornecedor criado' do
+      fornecedor = build(:fornecedor)
+      post fornecedores_path, params: { fornecedor: fornecedor.attributes }
+      fornecedor = Fornecedor.last
+      expect(response).to have_http_status(201)
+      expect(response.body).to include_json(fornecedor.as_json)
+    end
+  end
+
+  describe 'PUT /fornecedores/:id' do
+    it 'retorna código 201 e um json com fornecedor criado' do
+      fornecedor = Fornecedor.select(:id, :nome).last
+      fornecedor.nome = '0000'
+      attributes = fornecedor.attributes
+      attributes.delete('id')
+      put fornecedor_path(fornecedor), params: { fornecedor: attributes }
+      expect(response).to have_http_status(200)
+      expect(response.body).to include_json(attributes.as_json)
+    end
+  end
+
+  describe 'DELETE /fornecedores/:id' do
+    it 'retorna código 204 e body vazio' do
+      fornecedor = Fornecedor.last
+      delete fornecedor_path(fornecedor)
+      expect(response).to have_http_status(204)
+      expect(response.body).to eq('')
+    end
+  end
 end
