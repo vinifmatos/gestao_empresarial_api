d0f96a037e704e28eeaa1b5b87f15dd2e9de0769
vinifmatos <viniciusfreire4@gmail.com>
2020-02-24 23:39:45 -0300

    implementa as_json no model produto

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -3,4 +3,8 @@
 class Produto < ApplicationRecord
   validates :descricao, presence: true
   validates :preco, numericality: { greater_than: 0 }
+
+  def as_json(_options)
+    super(except: %i[created_at updated_at])
+  end
 end
