505d34a21df30924f910bc3f737194bf8806d7a9
vinifmatos <viniciusfreire4@gmail.com>
2020-02-27 22:56:58 -0300

    muda o metodo da rota de lsitar_clientes_por_nome para get

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   resources :produtos
   resources :endereco_clientes
   resources :clientes
-  post '/listar_clientes_por_nome', to: 'clientes#listar_por_nome', as: 'listar_por_nome'
+  get '/listar_clientes_por_nome', to: 'clientes#listar_por_nome', as: 'listar_por_nome'
 end
